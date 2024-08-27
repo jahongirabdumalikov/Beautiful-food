cfc6702fbd26f1bb1d92f646622350ff20a8d62b
jahongirabdumalikov <abdumalikovj015@gmail.com>
2024-08-27 10:49:29 -0700

    fc

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        "shadow": "box-shadow: 7px 7px 25px 0px #7E828F45"
+      },
+    },
+  },
+  plugins: [],
+}
+
